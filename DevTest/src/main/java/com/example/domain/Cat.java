3de253d8d696ed8a95d76bdba35fe7b6982a738d
student22 <hanssergiom@gmail.com>
2022-08-27 09:57:37 -0300

    start of the project, new cat user.

---
+++ DevTest/src/main/java/com/example/domain/Cat.java
@@ -0,0 +1,34 @@
+package com.example.domain;
+
+public class Cat {
+
+	private String id;
+	private String name;
+	
+	public Cat () {}
+
+	public Cat(String id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
+	
+}
